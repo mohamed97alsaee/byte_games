0dbc8bd1a07504cdd6f4f94245c06bb32aa9341b
Mohamed Alsaee <44772147+mohamed97alsaee@users.noreply.github.com>
2023-07-03 09:03:48 +0200

    localization added.

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,53 @@ import 'package:byte_games/providers/games_provider.dart';
 import 'package:byte_games/screens/auth_screens/splash_screen.dart';
 import 'package:byte_games/screens/handling_screens/loading_screen.dart';
 import 'package:byte_games/screens/main_screens/home_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'helpers/functions_helper.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key, this.locale});
+  final Locale? locale;
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Locale? _locale;
+
+  getLocaleLang() async {
+    if (widget.locale == null) {
+      String lang = await getStringToPres('localCode') ?? 'en';
+      setState(() {
+        _locale = Locale(lang);
+      });
+    } else {
+      setState(() {
+        _locale = widget.locale;
+      });
+    }
+    storeStringToPres('localCode', _locale!.languageCode.toString());
+  }
+
+  @override
+  void initState() {
+    getLocaleLang().then((value) {
+      if (kDebugMode) {
+        print("LOCALE IS : ${_locale!.languageCode.toString()}");
+      }
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +61,18 @@ class MyApp extends StatelessWidget {
             create: (context) => AuthProvider())
       ],
       child: MaterialApp(
+          locale: _locale,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en'), // English
+            Locale('ar'), // Arabic
+            Locale('es'), // spanish
+          ],
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
             primarySwatch: Colors.orange,
--- lib/screens/main_screens/home_screen.dart
+++ lib/screens/main_screens/home_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../widgets/game_card.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -25,17 +25,61 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Consumer<GamesProvider>(builder: (context, gamesConsumer, child) {
       return Scaffold(
-          floatingActionButton: FloatingActionButton(onPressed: () async {
-            final SharedPreferences prefs =
-                await SharedPreferences.getInstance();
-            prefs.clear().then((value) {
-              Navigator.pushAndRemoveUntil(
-                  context,
-                  CupertinoPageRoute(
-                      builder: (context) => const ScreenRouter()),
-                  (route) => false);
-            });
-          }),
+          appBar: AppBar(
+              title: Text(
+            AppLocalizations.of(context)!.hello,
+          )),
+          floatingActionButton: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                    child: const Text("EN"),
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => const MyApp(
+                                    locale: Locale('en'),
+                                  )),
+                          (route) => false);
+                    }),
+                FloatingActionButton(
+                    child: const Text("ES"),
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => const MyApp(
+                                    locale: Locale('es'),
+                                  )),
+                          (route) => false);
+                    }),
+                FloatingActionButton(
+                    child: const Text("AR"),
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => const MyApp(
+                                    locale: Locale('ar'),
+                                  )),
+                          (route) => false);
+                    }),
+                FloatingActionButton(onPressed: () async {
+                  final SharedPreferences prefs =
+                      await SharedPreferences.getInstance();
+                  prefs.clear().then((value) {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        CupertinoPageRoute(
+                            builder: (context) => const ScreenRouter()),
+                        (route) => false);
+                  });
+                }),
+              ],
+            ),
+          ),
           body: gamesConsumer.isLoading
               ? const Center(
                   child: CircularProgressIndicator(),
